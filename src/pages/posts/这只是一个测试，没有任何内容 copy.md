4203c7f0659148ebc6121cc01e9289f5cbe9b6c9
Qingke <ljw329426@163.com>
2025-05-12 22:29:18 +0800

    frame_finished

---
+++ src/pages/posts/这只是一个测试，没有任何内容 copy.md
@@ -0,0 +1,7 @@
+---
+layout: "../../layouts/MarkdownPostLayout.astro"
+title: "TEST1"
+pubDate: 1919/3/24
+auther: "青稞"
+type: "评测"
+---
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/pages/*.{html,js,astro}",
+    "./src/layouts/*.{html,js,astro}",
+  ],
+  theme: {
+    screens: {
+      // 完全覆盖默认断点
+      'xs': '350px',    // 折叠屏竖屏
+      'sm': '480px',    // 普通手机竖屏
+      'md': '680px',    // 折叠屏展开竖屏
+      'lg': '960px',    // 平板/手机横屏
+      'xl': '1200px',   // 平板横屏
+      'xxl': '1280px'   // 桌面设备
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
